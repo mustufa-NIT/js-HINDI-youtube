b4d46717149bd503e4240f6f8768b566eae74eb2
Mustufa_MNNIT <mustufaca060@gmail.com>
2026-03-07 19:52:44 +0000

    datatypes

---
+++ 01_besics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=202500060
+let accountEmail="Mustufa@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
