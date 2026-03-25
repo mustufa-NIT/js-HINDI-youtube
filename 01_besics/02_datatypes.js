1e633ad80763a9eeafa8ffafe8fcfdcbe579dcd7
Mustufa_MNNIT <mustufaca060@gmail.com>
2026-03-25 20:08:30 +0000

    datatypes

--- 01_besics/02_datatypes.js
+++ 01_besics/02_datatypes.js
@@ -1,2 +1,37 @@
+"use strict";//all js code as newer version (use strict likhte ke baad pura js ka code newer version ke trah treat karte hai magar aaj ka system ye automatic lag leta hai)//
+/* 1. 🔴 "use strict" ka matlab kya hai?
+
+👉 Ye JavaScript ka strict mode hai
+👉 Isse code zyada safe aur error-free ho jata hai */
+console.log(3+3);
+//alert(3+3); (we are using nodejs, not browser)//
+console.log(3+
+
+3)//code readability should be high//
+// console.log("Mustufa");
+// let name="Mustufa";
+// let age=22;
+// let isLogedIn=false;
+// let state;//undefine
+// let state2=NULL ;//NULL hai ya empty hai(null ek object hota hai)//
+
+//types of datatypes following//
+/* number => 2 power
+bigint=> for bigest number
+string="";
+boolean=> true/false;
+NULL=> Standalone value;
+symbol=> unique;
+object;
+*/
+console.log("Mustufa");
+console.log(typeof "Mustufa");
+console.log(typeof (3));
+console.log(typeof(true));
 console.log(typeof null);
 console.log(typeof undefined);
+
+
+
+
+
